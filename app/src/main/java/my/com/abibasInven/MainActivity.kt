1bddef7eff50f53fd55e40c075f0e9aa6f806001
BrendanX706 <48428673+BrendanX706@users.noreply.github.com>
2021-08-22 22:20:17 +0800

    Todo

--- app/src/main/java/my/com/abibasInven/MainActivity.kt
+++ app/src/main/java/my/com/abibasInven/MainActivity.kt
@@ -13,5 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //Todo
     }
 }
