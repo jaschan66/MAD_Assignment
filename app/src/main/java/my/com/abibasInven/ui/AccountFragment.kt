1c4a36398b45252cdc53657f604ce5c8538c5524
jaschan66 <57925035+jaschan66@users.noreply.github.com>
2021-09-04 00:48:01 +0800

    Changed are made to make it better (thanks to my boss ming zhi) respek

--- app/src/main/java/my/com/abibasInven/ui/AccountFragment.kt
+++ app/src/main/java/my/com/abibasInven/ui/AccountFragment.kt
@@ -1,8 +1,41 @@
 package my.com.abibasInven.ui
 
-
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import my.com.abibasInven.R
+import my.com.abibasInven.databinding.FragmentAccountBinding
 
-class AccountFragment:Fragment(R.layout.fragment_account) {
+
+class AccountFragment : Fragment() {
+
+    private lateinit var binding: FragmentAccountBinding
+    private val nav by lazy {findNavController()}
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = FragmentAccountBinding.inflate(inflater, container, false)
+
+
+        binding.bottomNavigationView.selectedItemId = R.id.account
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> nav.navigate(R.id.homeFragment)
+                R.id.account -> nav.navigate(R.id.accountFragment)
+                R.id.product -> nav.navigate(R.id.productFragment)
+
+            }
+            true
+        }
+
+
+
+        return binding.root
+    }
+
+
 }
--- app/src/main/java/my/com/abibasInven/ui/MainActivity.kt
+++ app/src/main/java/my/com/abibasInven/ui/MainActivity.kt
@@ -3,40 +3,23 @@ package my.com.abibasInven.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import my.com.abibasInven.R
 import my.com.abibasInven.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-
+    private val nav by lazy {supportFragmentManager.findFragmentById(R.id.host)!!.findNavController()}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val accountFragment= AccountFragment()
-        val homeFragment= HomeFragment()
-        val productFragment= ProductFragment()
-
-        setCurrentFragment(accountFragment)
-
-        binding.bottomNavigationView.setOnItemSelectedListener{
-            when(it.itemId){
-                R.id.account ->setCurrentFragment(accountFragment)
-                R.id.home ->setCurrentFragment(homeFragment)
-                R.id.product ->setCurrentFragment(productFragment)
-
-            }
-            true
-        }
-
     }
 
-    private fun setCurrentFragment(fragment: Fragment)=
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment,fragment)
-            commit()
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        return nav.navigateUp() || super.onSupportNavigateUp()
+    }
 }
