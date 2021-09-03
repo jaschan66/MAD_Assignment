4bcda7cc8f3bfd53e9ceab0272e3f320d7628a40
jaschan66 <57925035+jaschan66@users.noreply.github.com>
2021-09-04 00:48:58 +0800

    Changed are made to make it better (thanks to my boss ming zhi) respek

--- app/src/main/java/my/com/abibasInven/ui/ProductFragment.kt
+++ app/src/main/java/my/com/abibasInven/ui/ProductFragment.kt
@@ -1,7 +1,41 @@
 package my.com.abibasInven.ui
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import my.com.abibasInven.R
+import my.com.abibasInven.databinding.FragmentAccountBinding
+import my.com.abibasInven.databinding.FragmentProductBinding
 
-class ProductFragment:Fragment(R.layout.fragment_product) {
+
+class ProductFragment : Fragment() {
+
+    private lateinit var binding: FragmentProductBinding
+    private val nav by lazy {findNavController()}
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = FragmentProductBinding.inflate(inflater, container, false)
+
+        binding.bottomNavigationView.selectedItemId = R.id.product
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> nav.navigate(R.id.homeFragment)
+                R.id.account -> nav.navigate(R.id.accountFragment)
+                R.id.product -> nav.navigate(R.id.productFragment)
+
+            }
+            true
+        }
+
+
+        return binding.root
+    }
+
+
 }
