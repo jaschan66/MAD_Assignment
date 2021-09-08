6373f0c0e4bcb0c3ae3e0fe3889b9def11df0234
jaschan66 <57925035+jaschan66@users.noreply.github.com>
2021-09-08 21:55:50 +0800

    some modification made

---
+++ app/src/main/java/my/com/abibasInven/ui/StaffDetailsFragment.kt
@@ -0,0 +1,41 @@
+package my.com.abibasInven.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.logindemo.util.toBitmap
+import my.com.abibasInven.R
+import my.com.abibasInven.data.UserViewModel
+import my.com.abibasInven.databinding.FragmentHomeBinding
+import my.com.abibasInven.databinding.FragmentStaffDetailsBinding
+
+
+class StaffDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentStaffDetailsBinding
+    private val nav by lazy {findNavController()}
+    private val email by lazy { requireArguments().getString("email", null) }
+    private val vm: UserViewModel by activityViewModels()
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = FragmentStaffDetailsBinding.inflate(inflater, container, false)
+
+        // TODO
+        val s = vm.get(email)
+
+        binding.staffDetailImg.setImageBitmap(s?.photo?.toBitmap())
+        binding.lblDetailStaffName.setText(s?.name)
+        binding.lblRole.setText(s?.role)
+
+        binding.btnClose.setOnClickListener { nav.navigateUp() }
+
+        return binding.root
+    }
+
+}
--- app/src/main/java/my/com/abibasInven/ui/StaffListFragment.kt
+++ app/src/main/java/my/com/abibasInven/ui/StaffListFragment.kt
@@ -9,6 +9,8 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
+import com.example.logindemo.util.snackbar
+import com.google.firebase.auth.FirebaseAuth
 import my.com.abibasInven.R
 import my.com.abibasInven.data.UserViewModel
 import my.com.abibasInven.databinding.FragmentHomeBinding
@@ -16,15 +18,17 @@ import my.com.abibasInven.databinding.FragmentStaffListBinding
 import my.com.abibasInven.util.StaffAdapter
 
 
-class staffListFragment : Fragment() {
+class StaffListFragment : Fragment() {
 
     private lateinit var binding: FragmentStaffListBinding
     private val nav by lazy {findNavController()}
     private val vm: UserViewModel by activityViewModels()
 
+
     private lateinit var adapter: StaffAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
 
         binding = FragmentStaffListBinding.inflate(inflater, container, false)
 
@@ -35,12 +39,25 @@ class staffListFragment : Fragment() {
         adapter = StaffAdapter() { holder, user ->
 
             holder.root.setOnClickListener {
-                //nav.navigate()
+                nav.navigate(R.id.staffDetailsFragment, bundleOf("email" to user.email))
             }
             holder.btnUpdate.setOnClickListener {
                 nav.navigate(R.id.updateStaffFragment, bundleOf("email" to user.email))
             }
-            holder.btnDelete.setOnClickListener { deleteStaff(user.email) }
+            holder.btnDelete.setOnClickListener {
+                //TODO - NEED TO PRESS 2 TIMES IN ORDER TO DELETE THE SECOND RECORD
+                var auth : FirebaseAuth = FirebaseAuth.getInstance()
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(user.email, user.password)
+                val user1 = auth.currentUser
+                user1?.delete()
+                    ?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            snackbar("user deleted successfully")
+                            deleteStaff(user.email)
+                        }
+                    }
+
+            }
 
         }
         binding.staffrv.adapter = adapter
