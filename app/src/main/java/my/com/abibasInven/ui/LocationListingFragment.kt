a2a38f6110b088429d3c798585dac8a9e07c5b14
ntome19998 <73732118+ntome19998@users.noreply.github.com>
2021-09-08 16:25:07 +0800

    added (fragment) - locationAdding Fragment

--- app/src/main/java/my/com/abibasInven/ui/LocationListingFragment.kt
+++ app/src/main/java/my/com/abibasInven/ui/LocationListingFragment.kt
@@ -5,33 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import my.com.abibasInven.R
+import my.com.abibasInven.data.LocationViewModel
+import my.com.abibasInven.data.UserViewModel
 import my.com.abibasInven.databinding.FragmentHomeBinding
 import my.com.abibasInven.databinding.FragmentLocationListingBinding
+import my.com.abibasInven.util.LocationAdapter
+import my.com.abibasInven.util.StaffAdapter
 
 
 class LocationListingFragment : Fragment() {
 
     private lateinit var binding: FragmentLocationListingBinding
     private val nav by lazy {findNavController()}
+
+    private lateinit var adapter: LocationAdapter
+    private val vm: LocationViewModel by activityViewModels()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = FragmentLocationListingBinding.inflate(inflater, container, false)
 
         // TODO
 
-        binding.bottomNavigationView.selectedItemId = R.id.home
+        adapter = LocationAdapter() { holder, location ->
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> nav.navigate(R.id.homeFragment)
-                R.id.account -> nav.navigate(R.id.accountFragment)
-                R.id.product -> nav.navigate(R.id.productFragment)
-
+            holder.root.setOnClickListener {
+                //nav.navigate()
             }
-            true
+            holder.btnDetail.setOnClickListener {
+                nav.navigate(R.id.locationDetailsFragment, bundleOf("locationID" to location.ID))
+            }
+
         }
+        binding.rvLocation.adapter = adapter
+        binding.rvLocation.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+
+        vm.getAll().observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+            binding.lblLocationCount.text = "${it.size} rack(s)"
+        }
+
 
         return binding.root
     }
